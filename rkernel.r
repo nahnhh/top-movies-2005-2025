de979009f2c0357ec73161d37cb3ea7919c64700
nahnhh <hithnah@gmail.com>
2025-11-14 13:00:35 +0700

    Hân rescraped movies data :D

--- rkernel.r
+++
@@ -1,3 +0,0 @@
-install.packages('IRkernel')
-IRkernel::installspec(user = FALSE)  # to register the kernel in the current R installation
-jupyter labextension install @techrah/text-shortcuts  # for RStudio’s shortcuts
